033d9ec8a7773f895916a938c6924febc1f160c0
Dairo Mosquera <dairoandres115@outlook.com>
2025-03-13 00:19:16 -0500

    tests: remove unnecessary assertions

--- tests/test_services.py
+++ tests/test_services.py
@@ -58,29 +58,15 @@ def test_service_collection(test_client):
 
 
 def test_transient_service(test_client):
-    # first request
-    response1 = test_client.get('/test-services')
-    data1 = response1.json()
-    assert data1['greeter_id'] != data1['tester_ids']['greeter_id']
-
-    # second request
-    response2 = test_client.get('/test-services')
-    data2 = response2.json()
-    assert data2['greeter_id'] != data1['greeter_id']
-    assert data2['greeter_id'] != data2['tester_ids']['greeter_id']
+    response = test_client.get('/test-services')
+    data = response.json()
+    assert data['greeter_id'] != data['tester_ids']['greeter_id']
 
 
 def test_scoped_service(test_client):
-    # first request
-    response1 = test_client.get('/test-services')
-    data1 = response1.json()
-    assert data1['scoped_id'] == data1['tester_ids']['scoped_id']
-
-    # second request
-    response2 = test_client.get('/test-services')
-    data2 = response2.json()
-    assert data2['scoped_id'] != data1['scoped_id']
-    assert data2['scoped_id'] == data2['tester_ids']['scoped_id']
+    response = test_client.get('/test-services')
+    data = response.json()
+    assert data['scoped_id'] == data['tester_ids']['scoped_id']
 
 
 def test_singleton_service(test_client):
