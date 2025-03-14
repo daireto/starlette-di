8025209fd7642266ec201096ba46f7434280f98a
Dairo Mosquera <dairoandres115@outlook.com>
2025-03-14 06:41:12 -0500

    minor: add docstring to the package and remove classifier

--- starlette_di/__init__.py
+++ starlette_di/__init__.py
@@ -1,3 +1,14 @@
+"""Starlette DI is a dependency injection library for Starlette applications.
+
+It simplifies dependency management by allowing services to be injected using
+Scoped, Transient, and Singleton lifetimes (similar to .NET Core). Also,
+enables automatic injection of route parameters, and request bodies using
+Pydantic models, making API development more efficient, and structured.
+
+Visit the `repository <https://github.com/daireto/starlette-di>`_
+for more information.
+"""
+
 from .inject import inject, inject_class, inject_method
 from .middleware import DependencyInjectionMiddleware
 from .service_collection import ServiceCollection
@@ -14,4 +25,4 @@ __all__ = [
     'ServiceProvider',
 ]
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
